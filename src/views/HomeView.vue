5877889d89435d048abc4d43eff35726e59d43ad
sub._.odh <108208948+5u80dh@users.noreply.github.com>
2024-06-12 09:50:02 +0545

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+import GameCard from "../components/GameCard.vue";
+
+export default {
+  components: {
+    GameCard,
+  },
+  props: ["games"],
+};
+</script>
+
+<template>
+  <main
+    class="flex flex-col gap-14 w-screen min-h-screen items-center justify-center"
+  >
+    <div class="hero w-5/6 sm:w-1/2 rounded-3xl bg-neutral">
+      <div class="hero-content !w-full text-center text-neutral-content">
+        <div class="m-2">
+          <h1 class="mb-5 text-5xl font-bold">
+            Welcome!
+          </h1>
+          <p>
+          This Game is crated by Vanessa Sweeps and Team.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full flex flex-wrap items-center sm:justify-center flex-col sm:flex-row gap-10"
+    >
+      <GameCard v-for="game in games" :key="game.name" :game="game" />
+    </div>
+  </main>
+</template>
